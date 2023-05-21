91d788a9bfb4e605a825674bcac0ed4abf84e3ba
ogzcode <oguzguc44@gmail.com>
2023-05-21 11:10:19 +0300

    Basic directions written

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 import express from "express";
+import ejs from "ejs";
+
+import { getHomePage, getAboutPage, getAddPage, getPhotoPage } from "./controllers/pageControllers.js";
 
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
 
 app.get("/test", (req, res) => {
     res.send("Hello");
 });
+
+app.get("/", getHomePage);
+app.get("/about", getAboutPage);
+app.get("/add", getAddPage);
+app.get("/video-page", getPhotoPage);
 
 app.listen(3000, () => {
     console.log("Sunucu başlatıldı : http://localhost:3000/");
