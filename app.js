82c58c17eeee03ad2844664751bccb931c79d603
ogzcode <oguzguc44@gmail.com>
2023-05-22 13:04:28 +0300

    AddPhoto route added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import ejs from "ejs";
 
-import { getHomePage, getAboutPage, getAddPage, getPhotoPage } from "./controllers/pageControllers.js";
+import { getHomePage, getAboutPage, getAddPage, getPhotoPage, addPhoto } from "./controllers/pageControllers.js";
 
 const app = express();
 
 app.set("view engine", "ejs");
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/test", (req, res) => {
     res.send("Hello");
@@ -17,6 +20,9 @@ app.get("/", getHomePage);
 app.get("/about", getAboutPage);
 app.get("/add", getAddPage);
 app.get("/video-page", getPhotoPage);
+
+app.post("/photoAdd", addPhoto);
+
 
 app.listen(3000, () => {
     console.log("Sunucu başlatıldı : http://localhost:3000/");
