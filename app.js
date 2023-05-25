d6088ef9a943153878779a92ca88cb37db990ef3
ogzcode <oguzguc44@gmail.com>
2023-05-25 20:02:07 +0300

    MongoDB connection created and single photo route added

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import express from "express";
 import ejs from "ejs";
+import mongoose from "mongoose";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-import { getHomePage, getAboutPage, getAddPage, getPhotoPage, addPhoto } from "./controllers/pageControllers.js";
+import { getHomePage, getAboutPage, getAddPage } from "./controllers/pageControllers.js";
+import { addPhoto, getSinglePhoto } from "./controllers/photoControllers.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+mongoose.connect("mongodb://localhost/example", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,9 +31,9 @@ app.get("/test", (req, res) => {
 app.get("/", getHomePage);
 app.get("/about", getAboutPage);
 app.get("/add", getAddPage);
-app.get("/video-page", getPhotoPage);
 
 app.post("/photoAdd", addPhoto);
+app.get("/photo/:id", getSinglePhoto);
 
 
 app.listen(3000, () => {
