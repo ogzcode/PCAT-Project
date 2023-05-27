1461aab9a09ef7d3ed4bde169335f78522419150
ogzcode <oguzguc44@gmail.com>
2023-05-27 11:49:32 +0300

    Added Mutter middleware

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import ejs from "ejs";
-import mongoose from "mongoose";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import multer from "multer";
 
 import { getHomePage, getAboutPage, getAddPage } from "./controllers/pageControllers.js";
 import { addPhoto, getSinglePhoto } from "./controllers/photoControllers.js";
@@ -10,19 +10,24 @@ import { addPhoto, getSinglePhoto } from "./controllers/photoControllers.js";
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+export const __dirname = path.dirname(__filename);
 
-mongoose.connect("mongodb://localhost/example", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+const upload = multer({
+    storage: multer.diskStorage({
+        filename: (req, file, cb) => {
+            cb(null, file.originalname);
+        },
+        destination: (req, file, cb) => {
+            cb(null, "public/upload");
+        }
+    })
 });
 
 app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.get("/test", (req, res) => {
     res.send("Hello");
@@ -32,7 +37,7 @@ app.get("/", getHomePage);
 app.get("/about", getAboutPage);
 app.get("/add", getAddPage);
 
-app.post("/photoAdd", addPhoto);
+app.post("/photoAdd", upload.single("image"), addPhoto);
 app.get("/photo/:id", getSinglePhoto);
 
 
