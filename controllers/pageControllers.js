fed4ce73c723fd483cde8e2fe8e3864a4e17cedc
ogzcode <oguzguc44@gmail.com>
2023-05-29 09:44:01 +0300

    Added photo deletion route Added edit page route and added pagination for homepage

--- controllers/pageControllers.js
+++ controllers/pageControllers.js
@@ -1,8 +1,18 @@
 import { Photos } from "../models/Photo.js";
 
 export const getHomePage = async (req, res) => {
-    const photos = await Photos.find({});
-    res.render("index", { photos });
+    const page = req.query.page || 1;
+    const photosPerPage = 4;
+
+    const totalPhoto = await Photos.find({}).countDocuments();
+
+    const photos = await Photos.find({})
+        .skip((page - 1) * photosPerPage)
+        .limit(photosPerPage);
+
+    res.render("index", { photos: photos, current: page, pages: Math.ceil(totalPhoto / photosPerPage) });
+    /* const photos = await Photos.find({});
+    res.render("index", { photos }); */
 };
 
 export const getAboutPage = (req, res) => {
@@ -12,3 +22,8 @@ export const getAboutPage = (req, res) => {
 export const getAddPage = (req, res) => {
     res.render("add");
 };
+
+export const getEditPage = async (req, res) => {
+    const photo = await Photos.findOne({ _id: req.params.id });
+    res.render("edit", { photo });
+}
--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -1,13 +1,39 @@
 import { Photos } from "../models/Photo.js";
+import fs from "fs";
+import { __dirname } from "../app.js";
 
 export const addPhoto = async (req, res) => {
     await Photos.create({
         ...req.body,
         image: "/upload/" + req.file.originalname
-    });    
+    });
+    res.redirect("/")
 };
 
 export const getSinglePhoto = async (req, res) => {
     const photo = await Photos.findOne({ _id: req.params.id });
     res.render("photo", { photo });
+};
+
+export const deletePhoto = async (req, res) => {
+    const photo = await Photos.findOne({ _id: req.params.id });
+    const deletedImage = __dirname + "/public" + photo.image;
+
+    if (deletedImage && (await Photos.find({ image: photo.image })).length === 1) {
+        fs.unlinkSync(deletedImage);
+    }
+    
+    await Photos.findByIdAndRemove(req.params.id);
+
+    res.redirect("/");
+};
+
+export const editPhoto = async (req, res) => {
+    const photo = await Photos.findOne({ _id: req.params.id });
+
+    photo.title = req.body.title;
+    photo.description = req.body.description;
+    photo.save();
+
+    res.redirect(`/photo/${req.params.id}`);
 };
